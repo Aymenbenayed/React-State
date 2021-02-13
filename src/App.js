cc1d7fe6d434b4eae5ea00a2916943e504f67195
Aymen Ben Ayed <Aymen.bnmohamed@outlook.com>
2021-02-13 16:21:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import photo from "./photo.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Person: {
+      fullName: "Aymen Ben Ayed",
+      bio: "I'm curently student in UMT and i'm learning React in GoMyCode",
+      imgSrc:<img src={photo} alt="myImage"></img> ,
+      profession: "Student",
+    },
+    show: true,
+  };
+  handleClickShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show,
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <Button variant="info" onClick={this.handleClickShowPerson}>
+          Show Profile
+        </Button>
+        {this.state.show && (
+          <div className="profile">
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img width="20%" src={photo} alt="myImage"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
